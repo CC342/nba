ebcccf9172e253e41704d0d4ffcf1ddbca8fede4
CC342 <47269190+CC342@users.noreply.github.com>
2026-01-04 19:40:29 +0800

    Update other.py

--- other.py
+++ other.py
@@ -62,4 +62,4 @@ def direct_proxy(subpath):
         return f"Error: {e}", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000, threaded-True)
+    app.run(host='0.0.0.0', port=9000, threaded=True)
